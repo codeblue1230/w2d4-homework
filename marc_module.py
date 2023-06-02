119f50aae777435b7a6665011e18f00fcdb548a2
Marc Engle <marcengle30@gmail.com>
2023-06-02 00:31:03 -0400

    first commit

---
+++ marc_module.py
@@ -0,0 +1,12 @@
+import math
+
+#1B
+def area(length, width):
+    """Calculates square footage by multiplying a length and width"""
+    return length * width
+
+#1B
+def circ(radius):
+    """Takes in the radius and calculates circumfrence of a circle"""
+    return 2 * math.pi * radius
+
